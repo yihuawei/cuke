275e2891fa9eb1d2459d811c83d1bb8d36f48f6a
Yihua Wei <yihuawei@Yihuas-MacBook-Pro.local>
2023-10-09 22:46:48 -0500

    add lab2

---
+++ lab/lab2.py
@@ -0,0 +1,53 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from core.ir import *
+from codegen.cpu import *
+
+def PrintCCode(ir):
+	code = ''
+	for d in ir:
+		if d:
+			code += to_string(d)
+	print(code)
+
+def Loop0():
+    ir = []
+
+    L = Scalar('int', 'L')
+    M = Scalar('int', 'M')
+    N = Scalar('int', 'N')
+    A = Ndarray('int', (N, M, L), 'A')
+    B = Ndarray('int', (N, M, L), 'B')
+
+    loopi = Loop(0, 20, 1, [])
+    loopj = Loop(Expr(loopi.iterate, 3, '+'),  Expr(loopi.iterate, 10, '+'), 1, [])
+    loopk = Loop(Expr(loopj.iterate, 20, '+'), Expr(loopj.iterate, 30, '+'), 1, [])
+
+    loopi.body.append(loopj)
+    loopj.body.append(loopk)
+
+    lhs1 = Index(Index(Index(A, Expr(loopi.iterate, 1, '+')), loopj.iterate), loopk.iterate)
+    rhs1 = Index(Index(Index(B, loopi.iterate), loopj.iterate), loopk.iterate)
+	
+    # body = Assignment(lhs, Expr(rhs1, rhs2, '+'))
+    loopk.body.extend([Assignment(lhs1, Expr(rhs1, 2, '+'))])
+
+    ir.extend([Decl(L)])
+    ir.extend([Decl(M)])
+    ir.extend([Decl(N)])
+    ir.extend([Decl(A)])
+    ir.extend([Decl(B)])
+    ir.extend([loopi])
+
+    return ir
+
+
+def LoopTiling(loop_ir, tile_size = []):
+	#implement here
+	pass
+
+if __name__ == "__main__":
+    loop0_ir = Loop0()
+    PrintCCode(loop0_ir)
